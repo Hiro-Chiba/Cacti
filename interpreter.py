7e9129b90818627a647278b6b7d185bd3e7b3e4e
Hiro <Hiro.work1011@gmail.com>
2025-10-08 19:11:00 +0900

    演算子の優先順位を実装

--- interpreter.py
+++ interpreter.py
@@ -6,15 +6,9 @@ from lexer import INTEGER, PLUS, MINUS, ASTERISK, SLASH, EOF
 class Interpreter:
     def __init__(self, lexer):
         self.lexer = lexer
-        # 最初のトークンを取得してセットする
         self.current_token = self.lexer.get_next_token()
 
     def eat(self, token_type):
-        """
-        現在のトークンが期待する型(token_type)と一致すれば、
-        トークンを「消費」して、次のトークンに進める。
-        一致しなければエラーを発生させる。
-        """
         if self.current_token.type == token_type:
             self.current_token = self.lexer.get_next_token()
         else:
@@ -22,33 +16,48 @@ class Interpreter:
                 f"構文エラー: 期待していたトークンは {token_type}, 実際は {self.current_token.type}"
             )
 
-    def expr(self):
-        """
-        式の解析と評価を行う
-        expr -> INTEGER PLUS INTEGER
-        expr -> INTEGER MINUS INTEGER ...
-        """
-        # 最初のトークンは必ず数字であるはず
-        result = self.current_token.value
-        self.eat(INTEGER)
 
-        while self.current_token.type in (PLUS, MINUS, ASTERISK, SLASH):
+    def factor(self):
+        """factor : INTEGER"""
+        # 現状、因子は整数のみ
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
+
+    def term(self):
+        """term : factor ((ASTERISK | SLASH) factor)*"""
+        # 最初のfactorを取得
+        result = self.factor()
+
+        # '*' か '/' が続く限り、計算を続ける
+        while self.current_token.type in (ASTERISK, SLASH):
+            op = self.current_token
+            if op.type == ASTERISK:
+                self.eat(ASTERISK)
+                result = result * self.factor()
+            elif op.type == SLASH:
+                self.eat(SLASH)
+                # ゼロ除算のエラーチェック
+                divisor = self.factor()
+                if divisor == 0:
+                    raise Exception("エラー: 0で割ることはできません")
+                result = result / divisor
+
+        return result
+
+    def expr(self):
+        """expr : term ((PLUS | MINUS) term)*"""
+        # 最初のtermを取得
+        result = self.term()
+
+        # '+' か '-' が続く限り、計算を続ける
+        while self.current_token.type in (PLUS, MINUS):
             op = self.current_token
             if op.type == PLUS:
                 self.eat(PLUS)
-                result = result + self.current_token.value
-                self.eat(INTEGER)
+                result = result + self.term()
             elif op.type == MINUS:
                 self.eat(MINUS)
-                result = result - self.current_token.value
-                self.eat(INTEGER)
-            elif op.type == ASTERISK:
-                self.eat(ASTERISK)
-                result = result * self.current_token.value
-                self.eat(INTEGER)
-            elif op.type == SLASH:
-                self.eat(SLASH)
-                result = result / self.current_token.value
-                self.eat(INTEGER)
+                result = result - self.term()
 
         return result
